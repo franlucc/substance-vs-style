7b2d7c450fb0bad94ea22f57129e2b45bead21c8
franlucc <lucchetti.f@northeastern.edu>
2025-03-12 12:52:42 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/substance-vs-style/", // Replace with your actual repo name
+  base: "https://franlucc.github.io/substance-vs-style/", // Replace with your actual repo name
   plugins: [react()],
 });
