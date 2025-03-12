1ca57a7ce4812532d97909107a9c34a8713c40ef
franlucc <lucchetti.f@northeastern.edu>
2025-03-12 12:54:50 -0400

    vite config

--- src/GraphTabs.jsx
+++ src/GraphTabs.jsx
@@ -23,7 +23,7 @@ export default function GraphTabs() {
         <div className="flex-grow w-full overflow-auto">
           {selectedTab && (
             <iframe
-              src={`/graphs/${selectedTab}`}
+              src={`${import.meta.env.BASE_URL}graphs/${selectedTab}`}
               className="w-full border-none"
               style={{ height: "100vh", minWidth: "1400px", overflow: "auto" }}
             ></iframe>
